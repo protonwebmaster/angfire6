809c27a76e701b81579f00f8bd4f8d6ce50d5012
scarabator <dcl.fredb@18pixel.fr>
2019-11-29 15:06:03 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
 
+const routes: Routes = [
+        { path: '', component: HomeComponent },
+        { path: 'login', component: LoginComponent },
+        { path: '**', redirectTo: '' }
 
-const routes: Routes = [];
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
